f5d9018876b7156b392f447ae25ad806c23ef248
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2020-06-06 14:01:20 +0300

    v 1.0.1
    Connection error handling
    Small improvements

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import json
 import requests
 from time import sleep
 
-
 latest_video_url = ''
 
 
@@ -17,46 +16,46 @@ class Telegram:
         self.chat_id = get_config()["telegram_chai_id"]
 
     def send_message(self, msg):
-        requests.get(
-            f"https://api.telegram.org/bot{self.token}/sendMessage?text={msg}&chat_id={self.chat_id}")
+        requests.get(f"https://api.telegram.org/bot{self.token}/sendMessage?text={msg}&chat_id={self.chat_id}")
 
 
 class YouTubeVideos:
     def __init__(self):
         self.url = f"https://www.youtube.com/user/{get_config()['youtube_channel_username']}/videos"
+        self.latest_video_url = 'https://youtube.com/watch?'
 
     def rm_duplicates(self, items_list):
         return list(dict.fromkeys(items_list))
 
     def get_videos(self):
-        res = str(requests.get(self.url).content).split(' ')
-        youtube_urls = [i.replace('href="', 'https://youtube.com') for i in res if 'href="/watch?' in i]
-        return self.rm_duplicates(youtube_urls)
+        while True:
+            sleep(1)
+            res = requests.get(self.url)
+            if res.ok and len(res.content) != 0:
+                ress = str(res.content).split(' ')
+                youtube_urls = [i.replace('href="', 'https://youtube.com') for i in ress if 'href="/watch?' in i]
+                return self.rm_duplicates(youtube_urls)
 
 
 def main():
     global latest_video_url
 
+    t = Telegram()
+    y = YouTubeVideos()
+    
     try:
-        t = Telegram()
-        y = YouTubeVideos()
-
         while True:
             new_video_urls = y.get_videos()
+            new_latest_url = [un[:-1] for un in new_video_urls][:-1][0]
 
             if len(new_video_urls) != 0:
                 if len(latest_video_url) == 0:
-                    latest_video_url = [u[:-1] for u in new_video_urls][:-1][0]
-                    t.send_message(latest_video_url)
+                    latest_video_url = new_latest_url
 
-                new_latest_url = [un[:-1] for un in new_video_urls][:-1][0]
                 if new_latest_url != latest_video_url:
                     latest_video_url = new_latest_url
                     t.send_message(latest_video_url)
-            else:
-                main()
-
-            sleep(3600)
+            sleep(3)
     except Exception as e:
         t.send_message(str(e))
         main()
